92807bb72768bbe757b084aa0b09d703bc90971c
gozgof <gozgof@gmail.com>
2021-01-22 11:28:39 +0000

    c2

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from getFractionFromNatNum import getFractionFromNatNum
 import re
 str=input()
 m=re.search('([^/]+)/([0-9]+)([-+*/])([(0-9]+)/([0-9]+)',str)
-print(m.group(0))
-print(m.group(1))
-print(m.group(2))
-print(m.group(3))
-print(m.group(4))
-print(m.group(5))
+#print(m.group(0))
+#print(m.group(1))
+#print(m.group(2))
+#print(m.group(3))
+#print(m.group(4))
+#print(m.group(5))
 if(m.group(3)=="+"):
     print(addTwoFractions(int(m.group(1)),int(m.group(2)),int(m.group(4)),int(m.group(5))))
 elif(m.group(3)=="-"):
