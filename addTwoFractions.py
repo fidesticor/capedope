8c841fd02e9d13fb504d1e11b18c3010ebebbc11
gozgof <gozgof@gmail.com>
2021-01-29 16:12:15 +0000

    jkj

--- addTwoFractions.py
+++ addTwoFractions.py
@@ -2,6 +2,6 @@ from findLCMOfTwoIntegers import findLCMOfTwoIntegers
 from leadFractionToDenominator import leadFractionToDenominator
 from findGCFOfTwoIntegers import findGCFOfTwoIntegers
 def addTwoFractions(n1,d1,n2,d2):
-    r1=1 #replace me
+    r1=3 #replace me
     r2=2 #replace me
     return (r1,r2)
