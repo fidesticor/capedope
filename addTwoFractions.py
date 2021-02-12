50cd05b7bc0664c0116f3d0e0562aaaf2ddfcd87
vverhyyanoneti@gmail.com <vverhyyanoneti@gmail.com>
2021-02-12 15:03:25 +0300

    hhk

--- addTwoFractions.py
+++ addTwoFractions.py
@@ -2,6 +2,10 @@ from findLCMOfTwoIntegers import findLCMOfTwoIntegers
 from leadFractionToDenominator import leadFractionToDenominator
 from findGCFOfTwoIntegers import findGCFOfTwoIntegers
 def addTwoFractions(n1,d1,n2,d2):
-    r1=3 #replace me
-    r2=2 #replace me
-    return (r1,r2)
+  while (d1! = 0: and d2! = 0):
+    (n1,d1) = (n1%d1)
+    (n2,d2) = (n2%d2)
+  return n1,n2
+def add
+
+
