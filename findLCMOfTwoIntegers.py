35016e9bd53648fe8685b4bad43443d17989561b
egorchikg <guseve822@gmail.com>
2021-02-19 14:32:38 +0300

    hfgh

--- findLCMOfTwoIntegers.py
+++ findLCMOfTwoIntegers.py
@@ -1,4 +1,8 @@
 from findGCFOfTwoIntegers import findGCFOfTwoIntegers
 def findLCMOfTwoIntegers(a,b):
-    r=1 #replace me
+    r=1
+    while(True):
+      if(r%a== and r%b==0)
+        break
+      r+=1
     return r
